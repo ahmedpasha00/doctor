5da14164ee62557a1c19777c959cb38e1ad110b5
ahmedpasha00 <ahmedpasha@example.com>
2025-08-17 15:07:15 +0300

    resolve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print("ahmed");
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "EaraSoft",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    ),
+  );
 }
